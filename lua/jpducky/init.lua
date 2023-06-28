16d3c7502184e8f23bce0b0a711ae4e47bcbf12f
jpducky <34105363+JPDucky@users.noreply.github.com~>
2023-06-27 21:31:37 -0500

    packer autosetup

--- lua/jpducky/init.lua
+++ lua/jpducky/init.lua
@@ -1,2 +1,23 @@
 require("jpducky.remap")
 require("jpducky.set")
+
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+local packer_exists = vim.fn.empty(vim.fn.glob(install_path)) == 0
+
+if not packer_exists then
+    vim.api.nvim_echo({{'Installing packer.nvim', 'Type'}}, true, {})
+    vim.fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.api.nvim_command('packadd packer.nvim')
+end
+
+vim.api.nvim_exec(
+    [[
+    augroup Packer
+        autocmd!
+        autocmd BufWritePost init.lua PackerCompile
+    augroup end
+    ]],
+    false
+)
+
+require('jpducky.packer')
