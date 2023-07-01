7aae65d5fccc633514c1b175662f9d31d6457836
jpducky <34105363+JPDucky@users.noreply.github.com~>
2023-07-01 15:58:06 -0500

    update

--- init_nvim.sh
+++ init_nvim.sh
@@ -2,5 +2,5 @@
 
 # this script sets up the neovim config
 echo "Setting up neovim config"
-nvim -c "lua require('jpducky')" -c quit
-nvim -c "PackerSync" -c quit
+nvim --headless -c "luafile init.lua"
+nvim --headless +PackerInstall +qall
